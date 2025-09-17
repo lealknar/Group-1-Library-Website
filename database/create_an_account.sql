a2d834946642d29b6237ffe6209773c936b618f3
BertBert <Christiansarasata89@gmail.com>
2025-09-17 07:45:40 -0300

    Update create_an_account.sql

--- database/create_an_account.sql
+++ database/create_an_account.sql
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `fullname` varchar(100) NOT NULL,
-  `cp_number` varchar(20) DEFAULT NULL,
+  `mobile_number` varchar(20) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
